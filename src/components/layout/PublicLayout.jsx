4f2cce6e2bf318482253d0c8e2567f2cf027e9a3
Sabalah <sabalahabubakar41@gmail.com>
2026-05-14 07:11:11 +0300

    removed footer in chat

--- src/components/layout/PublicLayout.jsx
+++ src/components/layout/PublicLayout.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from '../common/Navbar';
 import Footer from '../common/Footer';
 import AnnouncementBanner from '../common/AnnouncementBanner';
 
+// Pages that need full viewport height with no footer
+const FULL_HEIGHT_ROUTES = ['/chat'];
+
 const PublicLayout = () => {
+  const { pathname } = useLocation();
+  const isFullHeight = FULL_HEIGHT_ROUTES.includes(pathname);
+
+  if (isFullHeight) {
+    return (
+      <div className="h-screen flex flex-col overflow-hidden bg-gray-50">
+        <AnnouncementBanner />
+        <Navbar />
+        <main className="flex-1 overflow-hidden">
+          <Outlet />
+        </main>
+        {/* No footer on full-height pages like chat */}
+      </div>
+    );
+  }
+
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
       <AnnouncementBanner />
